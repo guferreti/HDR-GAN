c02a82233b825720ce6c74db11b990234fa6dd97
guferreti <36612684+guferreti@users.noreply.github.com>
2024-06-12 14:09:52 -0500

    Update download_dataset.sh

--- dataset/download_dataset.sh
+++ dataset/download_dataset.sh
@@ -1,3 +1,4 @@
 wget -O kalantari_dataset.zip "https://www.robots.ox.ac.uk/~szwu/storage/18_hdr/kalantari_dataset.zip"
-unzip kalantari_dataset.zip
+mkdir kalantari_dataset
+unzip kalantari_dataset/kalantari_dataset.zip -d kalantari_dataset
 rm kalantari_dataset.zip
